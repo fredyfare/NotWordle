6095cba7ebc3114ded6275f506468b9bd9e64864
fredyfare <fredyfare@gmail.com>
2023-11-15 17:23:59 -0600

    flip animation working

--- src/components/Letter/Letter.jsx
+++ src/components/Letter/Letter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { AppContext } from "../../App";
 
 function Letter({ letterPos, attemptVal }) {
@@ -6,8 +6,6 @@ function Letter({ letterPos, attemptVal }) {
     useContext(AppContext);
 
   const letter = board[attemptVal][letterPos];
-
-  const [animationDelay, setAnimationDelay] = useState(0); // Estado para el retraso de la animación
 
   const correct = correctWord.toUpperCase()[letterPos] === letter;
   const almost =
@@ -21,17 +19,13 @@ function Letter({ letterPos, attemptVal }) {
     if (letter !== "" && !correct && !almost) {
       setDisabledLetters((prev) => [...prev, letter]);
     }
-
-    // Calcula el retraso basado en la posición y el intento
-    const delay = letterPos * 0.1 + attemptVal * 0.1;
-    setAnimationDelay(delay);
   }, [currAttempt.attempt]);
 
   return (
     <div
       className="letter"
       id={letterState}
-      style={{ animationDelay: `${animationDelay}s` }}
+      style={{ animationDelay: `${attemptVal * 0.1 + letterPos * 0.1}s` }}
     >
       {letter}
     </div>
