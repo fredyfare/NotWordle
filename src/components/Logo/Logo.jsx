9269ba61018e9f28c864f9b4fc272f47eb12147c
fredyfare <fredyfare@gmail.com>
2023-11-20 09:01:22 -0600

    Two logos

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function Logo() {
-  const logos = ["LIKEREACT", "WORLDWIDE", "NOTWORDLE"];
+  const logos = ["CHOCOLATE", "NOTWORDLE"];
   const correctLogo = "NOTWORDLE";
   const [logoIndex, setLogoIndex] = useState(0);
   const [logoLetters, setLogoLetters] = useState(logos[0].split(""));
